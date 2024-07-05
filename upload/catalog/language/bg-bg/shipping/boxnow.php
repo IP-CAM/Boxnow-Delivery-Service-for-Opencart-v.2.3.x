1c203481298b5a3fac2d4591f15bc047d04f5af1
radoslavboxnow <radoslav.radoslavov@boxnow.bg>
2024-07-05 11:17:38 +0200

    any apm initial branch

---
+++ upload/catalog/language/bg-bg/shipping/boxnow.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_title']       = 'Доставка с BOX NOW';
+$_['text_description'] = 'BOX NOW';
+$_['text_select_lockerid'] = 'Нужно е да изберете BOX NOW автомат, за да продължите.';
+$_['error_boxnow'] 	= 'Необходимо е да изберете BOX NOW автомат!';
+$_['selected_boxnow'] = 'Избран автомат:';
